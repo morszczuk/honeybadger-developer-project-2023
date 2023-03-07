66eb89e87c210d91779c6929f00d61c94424c494
Miron Marczuk <miron@secret-source.eu>
2023-03-07 19:22:17 +0100

    1 - basic test for the endpoint defined

---
+++ test/integration/spam_report_analysis_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class SpamReportAnalysisTest < ActionDispatch::IntegrationTest
+  test "report analysis identifies spam report" do
+    post report_spam_analysis_path, params: spam_payload
+    assert_response :success
+  end
+
+  private
+
+  def spam_payload
+    {
+      "RecordType": "Bounce",
+      "Type": "SpamNotification",
+      "TypeCode": 512,
+      "Name": "Spam notification",
+      "Tag": "",
+      "MessageStream": "outbound",
+      "Description": "The message was delivered, but was either blocked by the user, or classified as spam, bulk mail, or had rejected content.",
+      "Email": "zaphod@example.com",
+      "From": "notifications@honeybadger.io",
+      "BouncedAt": "2023-02-27T21:41:30Z",
+    }
+  end
+end
