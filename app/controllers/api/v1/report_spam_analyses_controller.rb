ae9913d8eeff53d35e08955883fe991c2e12449e
Miron Marczuk <miron@secret-source.eu>
2023-03-07 23:42:20 +0100

    1 - Basic validation of the payload added

--- app/controllers/api/v1/report_spam_analyses_controller.rb
+++ app/controllers/api/v1/report_spam_analyses_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::ReportSpamAnalysesController < ApplicationController
   def create
-    debugger
-    if params[:report]['Type'] == 'SpamNotification'
-      render json: { is_spam: true }
+    report_payload = ReportPayload.new(params[:report])
+    if report_payload.valid?
+      if params[:report]['Type'] == 'SpamNotification'
+        render json: { is_spam: true }
+      else
+        render json: { is_spam: false }
+      end
     else
-      render json: { is_spam: false }
+      render json: { errors: report_payload.errors.messages }, status: :bad_request
     end
   end
 end
